f52ac56dc801d23e2e7cc1f38e522be86ad34fac
elianenassif <nassif.eliane@gmail.com>
2016-04-06 14:50:44 +0300

    updates

--- config.js
+++ config.js
@@ -3,5 +3,5 @@
 module.exports = {
 	type: 'multi',
 	//folders: ['/sample1', '/sample3']
-	folders: ['/sample3']
+	folders: ['/sample1']
 };
--- sample1/config.js
+++ sample1/config.js
@@ -1,39 +1,36 @@
 'use strict';
 
 module.exports = {
-    serviceName: "sampleFailing",
-    serviceGroup: "test",
-    serviceVersion: 1,
-    requestTimeout: 30,
-    requestTimeoutRenewal: 5,
-    extKeyRequired: true,
+	serviceName: "sampleSuccess1",
+	serviceGroup: "test",
+	serviceVersion: 1,
+	servicePort: 2002,
+	requestTimeout: 30,
+	requestTimeoutRenewal: 5,
+	extKeyRequired: true,
 	main: 'index.js',
-    type: 'service',
-    prerequisites: {
-        cpu: '',
-        memory: ''
-    },
+	type: 'service',
+	prerequisites: {},
 	"awareness": true,
-    "errors": {
-        "600": "Database error",
-        "601": "Missing info"
-    },
-    "schema": {
-
-        "/getInfo": {
-            "_apiInfo": {
-                "l": "Get Info",
-                "group": "Information",
-                "groupMain": true
-            },
-            "email": {
-                "source": ['query.email'],
-                "required": false,
-                "validation": {
-                    "type": "string",
-                    "format": "email"
-                }
-            }
-        }
-    }
+	"errors": {
+		"600": "Database error",
+		"601": "Missing info"
+	},
+	"schema": {
+		"/getInfo": {
+			"_apiInfo": {
+				"l": "Get Info",
+				"group": "Information",
+				"groupMain": true
+			},
+			"email": {
+				"source": ['query.email'],
+				"required": false,
+				"validation": {
+					"type": "string",
+					"format": "email"
+				}
+			}
+		}
+	}
 };
